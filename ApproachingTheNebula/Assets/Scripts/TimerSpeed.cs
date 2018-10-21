28cfc7d3bf96a5626b1b648c6d199b70427c827f
Trystan May <trystanmay@gmail.com>
2018-10-21 09:51:38 -0500

    Added win text

--- ApproachingTheNebula/Assets/Scripts/TimerSpeed.cs
+++ ApproachingTheNebula/Assets/Scripts/TimerSpeed.cs
@@ -8,12 +8,23 @@ public class TimerSpeed : MonoBehaviour
 {
     const double init = 48.5, fin = -55.5;
     double distance;
+    bool passedTheStart;
+    Text resultsTextField;
+
 
     // Use this for initialization
     void Start ()
     {
         var txt = gameObject.GetComponent<Text>();
         txt.text = "Speed: 0c\nTime remaining: 3d";
+	Text[] textFields = Camera.allCameras[0].GetComponentsInChildren<Text>();
+	for (int i = 0; i < textFields.Length; i++ ) {
+		if (textFields[i].name == "Results") {
+			resultsTextField = textFields[i];
+			break;
+		}
+	}
+	passedTheStart = false;
     }
 	
 	// Update is called once per frame
@@ -28,6 +39,10 @@ public class TimerSpeed : MonoBehaviour
         txt.text = "S P E E D: " + PercentC(speed.speed) + "c" + Environment.NewLine +
                    "T E M P E R A T U R E: " + temp + " C" + Environment.NewLine +
                    "D I S T A N C E  T O  O R I O N: " + distance + "ly";
+	if ( distance == 0 && passedTheStart ) {
+		resultsTextField.text = "Orion Has Been Reached.\nPress [R] to restart.";
+	}
+	passedTheStart = true;
     }
 
     private static string PercentC(float speed)
