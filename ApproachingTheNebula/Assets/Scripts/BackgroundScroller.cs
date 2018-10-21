a7f574189c7e93d2a4bc8f7057f6dac450e4c386
IAmAPersson <iamaperson620@gmail.com>
2018-10-20 20:12:17 -0500

    Implemented slowdown with finalized map

--- ApproachingTheNebula/Assets/Scripts/BackgroundScroller.cs
+++ ApproachingTheNebula/Assets/Scripts/BackgroundScroller.cs
@@ -7,6 +7,7 @@ public class BackgroundScroller : MonoBehaviour {
 
     public float speed;
     Vector3 startPosition;
+    bool beginning = true;
 
 	// Use this for initialization
 	void Start () {
@@ -16,8 +17,15 @@ public class BackgroundScroller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (speed < 5)
+        if (speed < 5 && beginning)
             speed += 0.02f;
+        else
+            beginning = false;
         transform.Translate(new Vector3(-1,0,0) * speed * Time.deltaTime);
+        if (transform.position.x < -45 && speed > 0)
+            speed -= 0.02f;
+        if (speed < 0)
+            speed = 0;
+
 	}
 }
