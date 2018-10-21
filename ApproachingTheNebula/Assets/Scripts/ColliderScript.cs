c84af2ea07f89c833f066dc385dbd7afe10431ae
Trystan May <trystanmay@gmail.com>
2018-10-20 20:56:36 -0500

    Done some stuff

---
+++ ApproachingTheNebula/Assets/Scripts/ColliderScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D other) {
+	        //move along with platform
+	        if (other.transform.tag == "Collider") {
+		    // Collider2D.collider.gameObject.name 
+	            Debug.Log(gameObject.name + " hit " + other.collider.gameObject.name);
+	        }
+    }	
+
+}
